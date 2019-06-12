37b4312f8efdc2bc7e4783e324941dab9673e1bc
LauraHegay <sashanika79@gmail.com>
2019-06-13 00:14:29 +0300

    Изменила расстояние между колонками и алгоритм вычисления высоты колонки

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ window.renderStatistics = function (ctx, names, times) {
   var BAR_Y = 100;
   var BAR_WIDTH = 40;
   var BAR_HEIGHT = 150;
-  var BAR_GAP = 20;
+  var BAR_GAP = 50;
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -17,19 +17,23 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.fillText('Ура вы победили!', 130, 40);
   ctx.fillText('Список результатов:', 130, 60);
-
+  var NumWinner = 0;
   for (var i = 0; i < names.length; i++) {
+    if (times[i + 1] > times[NumWinner]) {
+      NumWinner = i + 1;
+    }
+  }
+  for (i = 0; i < names.length; i++) {
     var BAR_OPACITY = Math.floor(Math.random() * 10 + 1) / 10;
-
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255,' + BAR_OPACITY + ')';
     }
-    if (Math.floor(times[i]) >= 15000) {
+    if (i === NumWinner) {
       var BarCurrentHeight = BAR_HEIGHT;
     } else {
-      BarCurrentHeight = Math.floor(times[i] / 100);
+      BarCurrentHeight = Math.floor(times[i] * BAR_HEIGHT / times[NumWinner]);
     }
     ctx.fillRect(BAR_X, BAR_Y + BAR_HEIGHT - BarCurrentHeight, 40, BarCurrentHeight);
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
